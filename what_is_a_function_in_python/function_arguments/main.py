53301e01d3147ac2d3c2ab2aead66340ac87b6fd
way2geek11 <>
2026-06-15 16:26:42 +0000

    Function Arguments

---
+++ what_is_a_function_in_python/function_arguments/main.py
@@ -0,0 +1,8 @@
+def calculate_triangle_perimeter(a,b,c):
+    # Calculate perimeter by summing all sides
+    perimeter = a+b+c
+    # Print the calculated perimeter
+    print(f'The perimeter is {perimeter}')
+
+#Testing the result
+calculate_triangle_perimeter(2, 3, 5)
